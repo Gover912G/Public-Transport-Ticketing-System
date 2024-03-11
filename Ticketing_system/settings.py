349827093a1440b4942ea974153369ae0bf8c516
Gover912G <ngigipaul912@gmail.com>
2024-03-12 01:54:16 +0300

    mysqlclient database intergration

--- Ticketing_system/settings.py
+++ Ticketing_system/settings.py
@@ -83,10 +83,23 @@ WSGI_APPLICATION = 'Ticketing_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Public_transport',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
