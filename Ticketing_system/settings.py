78a40557b9a70e9e3188d8920f13ffa386aaae10
Gover912G <ngigipaul912@gmail.com>
2024-03-13 14:42:00 +0300

    .....

--- Ticketing_system/settings.py
+++ Ticketing_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!9hihswo4o-&mw79xss#1-wv@ds*qz&q*ai0csl-2-p(chn38%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost','netlify.com']
 
